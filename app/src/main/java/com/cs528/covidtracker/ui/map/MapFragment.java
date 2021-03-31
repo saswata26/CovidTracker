7d41559d3ee06b494b401e12c7a40a141c3142a2
Zander Bobronnikov <zander@Zanders-MBP.home>
2021-03-31 11:28:48 -0400

    Work

--- app/src/main/java/com/cs528/covidtracker/ui/map/MapFragment.java
+++ app/src/main/java/com/cs528/covidtracker/ui/map/MapFragment.java
@@ -1,4 +1,4 @@
-package com.cs528.covidtracker.ui.home;
+package com.cs528.covidtracker.ui.map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,22 +14,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.cs528.covidtracker.R;
 
-public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
+public class MapFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
